51e6759c9d00cd729a646a57f63321e558ae459a
Jeffrey van den Elshout <jeffrey.vd.elshout@gmail.com>
2021-03-22 09:45:13 +0100

    Fix folder structure

---
+++ N-QueensTests/NQueenSolverTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass()]
+    public class NQueenSolverTests
+    {
+        [TestMethod()]
+        public void GetNQueenSolutionTest_SizeBelow1()
+        {
+            Assert.ThrowsException<ArgumentException>(() => NQueenSolver.GetNQueenSolution(0));
+        }
+
+        [TestMethod()]
+        public void GetNQueenSolutionTest_Size4()
+        {
+            Position[] positions =
+            {
+                new Position(3, 4),
+                new Position(1, 3),
+                new Position(4, 2),
+                new Position(2, 1)
+            };
+            List<Position> values = NQueenSolver.GetNQueenSolution(4);
+            foreach (var position in positions)
+            {
+                Assert.IsTrue(values.Contains(position));
+            }
+        }
+
+        [TestMethod()]
+        public void GetNQueenSolutionTest_Size1()
+        {
+            List<Position> values = NQueenSolver.GetNQueenSolution(1);
+            Assert.IsTrue(values.Contains(new Position(1, 1)));
+        }
+
+        [TestMethod()]
+        public void GetNQueenSolutionTest_ImpossibleSizes()
+        {
+            Assert.IsFalse(NQueenSolver.GetNQueenSolution(2).Any());
+            Assert.IsFalse(NQueenSolver.GetNQueenSolution(3).Any());
+        }
+    }
+}
---
+++ N-QueensTests/PositionTests.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass()]
+    public class PositionTests
+    {
+        [TestMethod()]
+        public void PositionTest_ConstructorRowAndColumn()
+        {
+            Position position = new Position(1, 2);
+            Assert.AreEqual(position.Column, 1);
+            Assert.AreEqual(position.Row, 2);
+        }
+
+        [TestMethod()]
+        public void PositionTest_ConstructorDirection_DownLeft()
+        {
+            Position position = new Position(1, 2);
+            Position differentPosition = new Position(position, Direction.DownLeft);
+            Assert.AreEqual(differentPosition.Column, 1 - 1);
+            Assert.AreEqual(differentPosition.Row, 2 - 1);
+        }
+
+        [TestMethod()]
+        public void PositionTest_ConstructorDirection_UpLeft()
+        {
+            Position position = new Position(1, 2);
+            Position differentPosition = new Position(position, Direction.UpLeft);
+            Assert.AreEqual(differentPosition.Column, 1 - 1);
+            Assert.AreEqual(differentPosition.Row, 2 + 1);
+        }
+
+        [TestMethod()]
+        public void PositionTest_ConstructorDirection_UpRight()
+        {
+            Position position = new Position(1, 2);
+            Position differentPosition = new Position(position, Direction.UpRight);
+            Assert.AreEqual(differentPosition.Column, 1 + 1);
+            Assert.AreEqual(differentPosition.Row, 2 + 1);
+        }
+
+        [TestMethod()]
+        public void PositionTest_ConstructorDirection_DownRight()
+        {
+            Position position = new Position(1, 2);
+            Position differentPosition = new Position(position, Direction.DownRight);
+            Assert.AreEqual(differentPosition.Column, 1 + 1);
+            Assert.AreEqual(differentPosition.Row, 2 - 1);
+        }
+
+        [TestMethod()]
+        public void PositionTest_Equals()
+        {
+            Position position = new Position(1, 2);
+            Position position2 = new Position(1, 2);
+            Assert.AreEqual(position, position2);
+        }
+
+        [TestMethod()]
+        public void PositionTest_Equals_NotEqualValues()
+        {
+            Position position = new Position(1, 2);
+            Position position2 = new Position(2, 2);
+            Assert.AreNotEqual(position, position2);
+        }
+
+        [TestMethod()]
+        public void PositionTest_HashCode()
+        {
+            Position position = new Position(1, 2);
+            Assert.AreEqual(position.GetHashCode(), HashCode.Combine(position.Column, position.Row));
+        }
+    }
+}
